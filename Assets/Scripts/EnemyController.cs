5e5ed7fb0613e390392851241a5940d41e30954d
Jan Zajicek <jan.zajicek1@g.pslib.com>
2023-01-30 14:00:03 +0100

    change

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class EnemyController : NetworkBehaviour
+{
+    [SerializeField] private GameObject enemySpawner;
+    [SyncVar] private int health = 1;
+    private float speed = 3f;
+    private int damage = 1;
+    private bool alreadyHit = false;
+    private Vector2 destination;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(destination != null)
+        {
+            GetComponent<Rigidbody2D>().velocity = transform.right * speed;
+        }
+    }
+    [Server]
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            enemySpawner.GetComponent<EnemySpawner>().EnemyCount -= 1;
+            Destroy(gameObject);
+        }
+    }
+    public void SetDestination(Vector2 dest)
+    {
+        destination = dest;
+        transform.right = destination;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            //without this player takes 2 damage instead of 1
+            if (alreadyHit) return;
+            alreadyHit = true;
+            gameObject.SetActive(false);
+            collision.GetComponent<PlayerController>().TakeDamage(damage);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Spawner"))
+        {
+            //Destroy(gameObject);
+            transform.right = -destination;
+            GetComponent<Rigidbody2D>().velocity = transform.right * speed;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class EnemySpawner : NetworkBehaviour
+{
+    //?? why
+    public int EnemyCount { get { return enemyCount; } set { enemyCap = value; } } 
+
+    [SerializeField] private GameObject pfEnemy;
+    private BoxCollider2D boxCollider;
+    private Bounds colliderBounds;
+    private Vector3 colliderCenter;
+    private int enemyCap = 20;
+    private int enemyCount = 0;
+    private float spawnTimer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        colliderBounds = boxCollider.bounds;
+        colliderCenter = colliderBounds.center;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(spawnTimer <= 0)
+        {
+            spawnTimer = Random.Range(.1f, 2);
+            if(enemyCount < enemyCap)
+            {
+                SpawnEnemy();
+                enemyCount++;
+            }
+        }
+        spawnTimer -= Time.deltaTime;
+
+        /*if (Input.GetKeyDown(KeyCode.X))
+        {
+            SpawnEnemy();
+        }*/
+    }
+    [Server]
+    public void SpawnEnemy()
+    {
+        float[] floats = { 
+            colliderCenter.x - colliderBounds.extents.x,
+            colliderCenter.x + colliderBounds.extents.x,
+            colliderCenter.y - colliderBounds.extents.y,
+            colliderCenter.y + colliderBounds.extents.y 
+        };
+        float randomX = Random.Range(floats[0], floats[1]);
+        float randomY = Random.Range(floats[2], floats[3]);
+        Vector3 spawnPos = new Vector3(randomX, randomY, 0);
+       
+        randomX = randomX - Random.Range(floats[0], floats[1])/2;
+        randomY = randomY - Random.Range(floats[2], floats[3])/2;
+        Vector3 destination = new Vector3(-randomX, -randomY);
+
+        var enemy = Instantiate(pfEnemy, spawnPos, Quaternion.identity);
+
+        enemy.GetComponent<EnemyController>().SetDestination(destination);
+
+        NetworkServer.Spawn(enemy);
+    }
+}
